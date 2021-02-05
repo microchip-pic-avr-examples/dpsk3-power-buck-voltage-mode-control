65727daa1077a5eb9d23670d0e7b0b244705fdb4
Microchip Technology <microchip@microchip.com>
2021-02-05 09:27:22 +0000

    1.0.0

--- .mchp_private/subtree/subtree-buck-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo $SSH_AUTH_SOCK
-cd ../../
-git subtree push --prefix="dpsk_vmc.X/sources/power_control/devices" subrepo-buck feature/version-update --squash
-echo Press Enter to exit
-read
-
